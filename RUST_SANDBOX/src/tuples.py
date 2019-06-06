3bebde6a86be848491d7a4d6503e6372e25d9a74
leekheang <ly.kheang.jr@gmail.com>
2019-06-06 23:41:56 +0700

    tuple-rs-py

--- RUST_SANDBOX/src/tuples.py
+++ RUST_SANDBOX/src/tuples.py
@@ -2,4 +2,4 @@
 tuplepy =   ("Lee", "Cambodia", 22)
 print(tuplepy)
 print("%s is from %s and %d" % tuplepy)
-# println!("{} is from {} and is {}", person.0, person.1, person.2);clear
+print (' {0[0]} is from {0[1]} and {0[2]}'.format(tuplepy))
